1498cba74236b21206dcd6f16538b93be4c08d8c
Lunia-UK <57706009+Lunia-UK@users.noreply.github.com>
2020-03-11 11:16:17 +0000

    Adding a loop to multiply the particle. Add random function for the color and position of the particles

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 let container = document.querySelector('#container')
-
 let renderer = new THREE.WebGLRenderer()
 renderer.setSize ( window.innerWidth, window.innerHeight)
 renderer.setSize( window.innerWidth, window.innerHeight )
 container.appendChild( renderer.domElement )
-
 let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,10000)
 scene.add(camera)
+let distance = 1000
 
-let geometry = new THREE.SphereGeometry( 50, 32, 32 );
-const material = new THREE.MeshBasicMaterial({ color: 0xfff000 })
-let particule = new THREE.Mesh( geometry, material )
-particule.position.x = 0
-particule.position.y = 0
-particule.position.z = 0
-scene.add( particule );
+for( let i=0; i<50; i++){
+    let geometry = new THREE.SphereGeometry( 5, 32, 32 );
+    let material = new THREE.MeshBasicMaterial( {color: Math.random() * 0x808080 + 0x808080} );
+    let particule = new THREE.Mesh( geometry, material );
+    particule.position.x = Math.random() * distance * 2 - distance
+    particule.position.y = Math.random() * distance * 2 - distance
+    particule.position.z = Math.random() * distance * 2 - distance
+    particule.scale.x = particule.scale.y = Math.random() * 10 + 5
+    scene.add( particule );
+}
 
 camera.position.z = 100
-camera.lookAt(particule)
+
 renderer.render(scene, camera)
