26eb63bd12050434e842d4f75ab529c1c048873d
Lunia-UK <57706009+Lunia-UK@users.noreply.github.com>
2020-03-11 16:02:58 +0000

    Correction BufferGeometry (line). Change camera position and distance variable.

--- js/main.js
+++ js/main.js
@@ -4,36 +4,34 @@ renderer.setSize ( window.innerWidth, window.innerHeight)
 renderer.setSize( window.innerWidth, window.innerHeight )
 container.appendChild( renderer.domElement )
 let scene = new THREE.Scene()
+scene.background = new THREE.Color( 0xffffff );
 let camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,10000)
 scene.add(camera)
-let distance = 1000
-var points = [];
+let distance = 500
+let points = [];
 
-for( let i=0; i<50; i++){
-    let geometry = new THREE.SphereGeometry( 5, 32, 32 );
+for( let i=0; i<70; i++){
+    let geometry = new THREE.SphereGeometry( Math.random() * 10 + 5, 32, 32 );
     let material = new THREE.MeshBasicMaterial( {color: Math.random() * 0x808080 + 0x808080} );
-    let particule = new THREE.Mesh( geometry, material );
-    particule.position.x = Math.random() * distance * 2 - distance
-    particule.position.y = Math.random() * distance * 2 - distance
-    particule.position.z = Math.random() * distance * 2 - distance
-    particule.scale.x = particule.scale.y = Math.random() * 10 + 5
-
-    points.push( new THREE.Vector3( particule.position ) );
-    scene.add( particule );
+    let particle = new THREE.Mesh( geometry, material );
+    particle.position.x = Math.random() * distance * 2 - distance
+    particle.position.y = Math.random() * distance * 2 - distance
+    particle.position.z = Math.random() * distance * 2 - distance
+    
+    points.push( new THREE.Vector3( particle.position.x, particle.position.y, particle.position.z ) );
+    scene.add( particle );
 }
 
-var geometryLine = new THREE.BufferGeometry().setFromPoints( points );
-
-let line = new THREE.Line (geometryLine, new THREE.LineBasicMaterial
-    ({
-        color: 0xffffff,
-        opacity: 0.05
-    }))
+let materialLine = new THREE.LineBasicMaterial( {
+    color: 0x000000,
+    opacity: 0.08, 
+    transparent: true,
+} );
+let geometryLine = new THREE.BufferGeometry().setFromPoints( points );
+let line = new THREE.Line (geometryLine, materialLine)
 scene.add(line)
 
-camera.position.z = 150
-
-renderer.render(scene, camera)
+camera.position.z = 1000
 
 document.addEventListener('mousemove', onMouseMove, false)
 
